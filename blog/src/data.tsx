3fcc6c8e0dfa0a8935d36025c5d3860d1834a50e
maoshuochen <maoshuochen@gmail.com>
2024-09-15 01:26:17 +0800

    ``` 更新PaperClip文章的特色图片链接和Vite配置
    重命名了PaperClip文章的特色图片文件，并更新了data.tsx中的图片链接，以反映新的文件路径。此外，修改了vite.config.ts，移除了'base'配置项的值，这将默认设置应用于服务静态文件。
    ```

--- blog/src/data.tsx
+++ blog/src/data.tsx
@@ -2,7 +2,7 @@ export const articles = [
     {
         key: "1",
         title: "PaperClip, Paper Knowledge Management Tool",
-        image_url: "../src/assets/paperclip/1656898314-Top-View-Grid.webp",
+        image_url: "/paperclip/paperclip-cover.webp",
     },
     {
         key: "2",
