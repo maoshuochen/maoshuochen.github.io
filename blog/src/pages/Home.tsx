c215f53a51f7330b5242981325eb420990d78188
maoshuochen <maoshuochen@gmail.com>
2025-03-27 22:52:59 +0800

    update: 修改首页为grid布局

--- blog/src/pages/Home.tsx
+++ blog/src/pages/Home.tsx
@@ -3,36 +3,38 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
     const covers = articles.map((article) => (
-        <div className="w-full space-y-4 cursor-pointer">
+        <div key={article.id} className="w-full cursor-pointer">
             <Link to={`/post/${article.id}`}>
-                <div className="flex flex-col space-y-4">
-                    <div className="flex w-full space-x-4 pt-4 items-center">
-                        <h2 className="text-2xl font-sans font-medium">
-                            {article.title}
-                        </h2>
-                        <h3 className="text-xl font-light">
-                            {article.subtitle}
-                        </h3>
-                    </div>
-                    <div
-                        className="
-                            w-full h-96 p-12
-                            rounded-3xl
+                <div
+                    className="
+                            w-full h-72 p-12
+                            rounded-xl
                             bg-white bg-cover 
                             border border-zinc-100  
                             hover:shadow-xl hover:shadow-zinc-200 duration-300"
-                        style={{
-                            backgroundImage: `url(${article.image_url})`,
-                        }}
-                    ></div>
+                    style={{
+                        backgroundImage: `url(${article.image_url})`,
+                    }}
+                ></div>
+                <div className="flex flex-col w-full space-y-2 pt-4 items-start">
+                    <h2 className="text-2xl font-sans font-medium">
+                        {article.title}
+                    </h2>
+                    <h3 className="text-xl font-light">{article.subtitle}</h3>
                 </div>
             </Link>
         </div>
     ));
 
     return (
-        <div className="flex h-full w-full justify-center pt-4">
-            <div className="w-3/4 flex flex-col space-y-16 p-8">{covers}</div>
+        <div className="flex flex-col h-full w-full justify-center items-center pt-4">
+            <div className="w-3/4 p-8">
+                <h1 className="text-4xl/relaxed font-sans font-medium py-20">
+                    Hi, I am Maoshuo Chen
+                    <br />I am a Product Designer
+                </h1>
+            </div>
+            <div className="w-3/4 grid grid-cols-2 gap-16 p-8">{covers}</div>
         </div>
     );
 }
